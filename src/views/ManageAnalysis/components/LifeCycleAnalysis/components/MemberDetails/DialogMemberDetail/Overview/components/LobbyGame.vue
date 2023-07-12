65e615efcfbdcf8bdc302178f52565b7bf5bf519
alvin_liu <alvin_liu@mail.chungyo.net>
2023-07-12 17:38:48 +0800

    ***調整內容：*** --------------------------------------- 1.會員經營分析 -> 趨勢分析 -> 測試項目：21點修正 2.新增處理Pie和Polar圖表的dataLabels顯示方式

--- src/views/ManageAnalysis/components/LifeCycleAnalysis/components/MemberDetails/DialogMemberDetail/Overview/components/LobbyGame.vue
+++ src/views/ManageAnalysis/components/LifeCycleAnalysis/components/MemberDetails/DialogMemberDetail/Overview/components/LobbyGame.vue
@@ -11,6 +11,7 @@ import {
   getHallCurrencySign,
   FormatNumber
 } from '@/utils/commonUtils.js'
+import { showPieDatasetsLabels } from '@/utils/pluginUtils.js'
 import { chart_fixed_bgColor } from '@/../public/js/system_config.js'
 import { ElNotification } from 'element-plus'
 import CdpMessage from '@/components/CdpMessage.vue'
@@ -34,6 +35,9 @@ const messageKey = ref('shortLoading')
 const refChart = ref(null)
 let chart
 
+const chartTotal = ref(0) //圓餅圖資料總和
+const pieSliceCount = ref(0) //圓餅圖切片傯數
+
 const chartSetting = {
   id: 'lobbyGameChart',
   type: 'doughnut',
@@ -51,14 +55,21 @@ const chartSetting = {
         formatter: function (value) {
           return FormatNumber(value)
         },
-        display: 'auto',
+        // display: 'auto',
         color: '#000',
         font: {
           weight: 'bold'
         },
         padding: 10,
         clamp: true,
-        clip: true
+        clip: true,
+        display: function (context) {
+          return showPieDatasetsLabels({
+            currentData: context.dataset.data[context.dataIndex],
+            dataTotal: chartTotal.value,
+            pieSliceCount: pieSliceCount.value
+          })
+        }
       },
       legend: {
         position: 'right',
@@ -140,6 +151,7 @@ const transformLobbyGameChart = (data) => {
     let label = data[i].lobby_name + '-' + data[i].game_name
     chartLabels.push(label)
     chartData.push(parseFloat(data[i].total_bet_amount))
+    chartTotal.value = chartTotal.value + parseFloat(data[i].total_bet_amount)
 
     let color = ''
     let borderColor = ''
@@ -157,6 +169,8 @@ const transformLobbyGameChart = (data) => {
     chartDataBgColor.push(color)
     chartDataBorderColor.push(borderColor)
   }
+
+  pieSliceCount.value = chartLabels.length
   chartSetting.data.labels = []
   chartSetting.data.labels = chartLabels
 
