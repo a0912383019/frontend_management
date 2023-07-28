40504e6d65f5370791f71d2754a6ada471e3d957
alvin_liu <alvin_liu@mail.chungyo.net>
2023-07-28 11:47:35 +0800

    ***調整內容：*** --------------------------------------- 1.會員旅程 -> 歷程紀錄 ：再次調整進階篩選不會更新問題

--- src/components/Dialog/DialogMemberDetail/Journey/components/MemberLifeCycleHistory.vue
+++ src/components/Dialog/DialogMemberDetail/Journey/components/MemberLifeCycleHistory.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useDialogMemberDetailStore } from '@/stores/dialogMemberDetail.js'
 import { storeToRefs } from 'pinia'
@@ -13,11 +13,21 @@ const { dialogMemberDetailRangeDate } = storeToRefs(dialogMemberDetailStore)
 
 const refLifeCycleHistory = ref(null)
 
+const chartIsShow = ref(false)
+
+onMounted(() => {
+  chartIsShow.value = true
+})
+
 watch(
   () => dialogMemberDetailRangeDate.value,
   () => {
-    refLifeCycleHistory.value.clearChart()
-    refLifeCycleHistory.value.queryChartApi()
+    chartIsShow.value = false
+    setTimeout(() => {
+      chartIsShow.value = true
+    }, 10)
+    // refLifeCycleHistory.value.clearChart()
+    // refLifeCycleHistory.value.queryChartApi()
   }
 )
 </script>
@@ -29,6 +39,7 @@ watch(
       :memberId="dialogMemberDetailStore.memberData.user_id"
       :detailDate="dialogMemberDetailRangeDate"
       ref="refLifeCycleHistory"
+      v-if="chartIsShow"
     />
   </section>
 </template>
