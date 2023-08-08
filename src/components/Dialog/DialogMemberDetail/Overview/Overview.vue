38fab6d31b3cbe36164aaa0b4fbcd346d9bc97a7
alvin_liu <alvin_liu@mail.chungyo.net>
2023-08-08 14:36:47 +0800

    ***調整內容：*** ---- 完成會員詳細資料，另開視窗

--- src/components/Dialog/DialogMemberDetail/Overview/Overview.vue
+++ src/components/Dialog/DialogMemberDetail/Overview/Overview.vue
@@ -10,23 +10,23 @@ import LobbyGame from './components/LobbyGame.vue'
 <template>
   <div>
     <el-row :gutter="15" class="mb-20">
-      <el-col :span="5">
+      <el-col :xs="24" :sm="24" :lg="5">
         <MemberHealth />
       </el-col>
-      <el-col :span="19">
+      <el-col :xs="24" :sm="24" :lg="19">
         <section class="cdp-section">
           <MemberInfo />
           <MemberAmount />
         </section>
       </el-col>
-      <el-col :span="24" class="mt-30">
+      <el-col :xs="24" :sm="24" :lg="24" class="mt-30">
         <MemberPeriodBetAmount />
       </el-col>
-      <el-col :span="24" class="mt-30">
+      <el-col :xs="24" :sm="24" :lg="24" class="mt-30">
         <el-row :gutter="20">
-          <el-col :span="8"> <LobbyGroup /> </el-col>
-          <el-col :span="8"> <Lobby /> </el-col>
-          <el-col :span="8"> <LobbyGame /> </el-col>
+          <el-col :xs="24" :sm="24" :lg="8"> <LobbyGroup /> </el-col>
+          <el-col :xs="24" :sm="24" :lg="8"> <Lobby /> </el-col>
+          <el-col :xs="24" :sm="24" :lg="8"> <LobbyGame /> </el-col>
         </el-row>
       </el-col>
     </el-row>
