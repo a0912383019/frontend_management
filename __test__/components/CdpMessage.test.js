e50e93223ac1d4b59035604957b8d47c52582218
alvin_liu <alvin_liu@mail.chungyo.net>
2023-09-12 09:44:38 +0800

    update: 組件測試

---
+++ __test__/components/CdpMessage.test.js
@@ -0,0 +1,58 @@
+import { test, describe, expect, beforeEach } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import { i18n } from '@/global/i18n'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import CdpMessage from '@/components/CdpMessage.vue'
+
+describe('CdpMessage', () => {
+  let wrapper = null
+
+  beforeEach(() => {
+    wrapper = shallowMount(CdpMessage, {
+      props: {
+        messageKey: 'shortLoading',
+        cover: true,
+        height: 300,
+        bg: 'dark'
+      },
+      global: {
+        plugins: [i18n],
+        components: {
+          FontAwesomeIcon
+        }
+      }
+    })
+  })
+
+  test('確認組件是否存在', () => {
+    expect(wrapper.classes('message')).toBe(true)
+  })
+
+  test('測試getIcon', () => {
+    const result = wrapper.vm.getIcon('loading')
+    expect(result).toBe('fa-solid fa-spinner')
+  })
+
+  test('測試messageData', () => {
+    const result = wrapper.vm.messageData
+    expect(result).toEqual({
+      icon: 'fa-solid fa-spinner',
+      title: '查詢中'
+    })
+  })
+
+  test('測試messageStyle', () => {
+    const result = wrapper.vm.messageStyle
+    expect(result).toEqual({
+      height: '300px'
+    })
+  })
+
+  test('測試props cover', () => {
+    expect(wrapper.find('.message').classes()).toContain('cover')
+  })
+
+  test('測試props bg', () => {
+    expect(wrapper.find('.message').classes()).toContain('dark')
+  })
+})
