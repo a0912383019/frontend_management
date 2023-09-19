0f6acd7d2d8d5e7e5a23e3dad9f2dd4c6ea3acb2
a0912383019 <a0912383019@gmail.com>
2023-09-19 16:17:21 +0800

    update:unitTest & add comma

---
+++ __test__/components/GenerateTagsBadge.test.js
@@ -0,0 +1,65 @@
+import { it, describe, expect, beforeEach, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import GenerateTagsBadge from '@/components/GenerateTagsBadge.vue'
+import * as module from '@/utils/commonUtils.js'
+import ElementPlus from 'element-plus'
+
+describe('GenerateTagsBadge', () => {
+  let wrapper = null
+  let spy
+  beforeEach(() => {
+    spy = vi.spyOn(
+      module,
+      'getSessionStorageEntity'
+    ).mockImplementation(vi.fn());
+
+    //模擬第一次與第二次呼叫getSessionStorageEntity
+    module.getSessionStorageEntity
+      .mockReturnValueOnce({
+        'tags_config': {
+          'esb': {
+            '30001': {
+              'tag_type': 1,
+            },
+          }
+        }
+      })
+      .mockReturnValueOnce({
+        'tags_config': {
+          'esb': {
+            '30001': {
+              'tag_type': 2,
+            },
+          }
+        }
+      })
+  })
+
+  it('掛載元件時是否有根據tag_type的不同去切換class', async () => {
+    //第一次掛載wrapper
+    wrapper = mount(GenerateTagsBadge, {
+      props: {
+        hall_name: 'esb',
+        tag_code: '30001',
+        badge_text_class: 'pink'
+      },
+      global: {
+        plugins: [ElementPlus]
+      }
+    })
+    expect(wrapper.vm.badge_class).toBe('badge badge-custom-danger pink')
+
+    //第二次掛載wrapper
+    wrapper = mount(GenerateTagsBadge, {
+      props: {
+        hall_name: 'esb',
+        tag_code: '30001',
+        badge_text_class: 'pink'
+      },
+      global: {
+        plugins: [ElementPlus]
+      }
+    })
+    expect(wrapper.vm.badge_class).toBe('badge badge-custom-green pink')
+  })
+})
