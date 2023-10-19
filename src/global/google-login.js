ec6573fd2d4fd8758fd20a9002207560bac4da33
alvin_liu <alvin_liu@mail.chungyo.net>
2023-10-19 17:01:43 +0800

    update: 修改不同環境的google OAUTH，開發環境提示改到storage

--- src/global/google-login.js
+++ src/global/google-login.js
@@ -1,8 +1,18 @@
 import vue3GoogleLogin from 'vue3-google-login'
 
+// 開發環境
+const developmentEnvironment = import.meta.env.VITE_ENV
+
+let clientId = '172879218577-i6b4q5765l7vu2qdf4d00d5p13604rv3.apps.googleusercontent.com'
+
+// for it, qa
+if (developmentEnvironment === 'qa' || developmentEnvironment === 'it') {
+  clientId = '1055824448864-oifmo3rcb9t51lucpbvsf6v0mhrfvh0s.apps.googleusercontent.com'
+}
+
 export const googleLoginRegister = (app) => {
   app.use(vue3GoogleLogin, {
-    clientId: '172879218577-i6b4q5765l7vu2qdf4d00d5p13604rv3.apps.googleusercontent.com',
+    clientId,
     buttonConfig: {
       theme: 'filled_blue',
       shape: 'pill',
