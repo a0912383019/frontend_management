6f0d99dca34addcac26b5b953c25c6fe24eacc39
alvin_liu <alvin_liu@mail.chungyo.net>
2023-10-30 10:03:06 +0800

    update: 單元測試

--- __test__/components/Filter/UploadFile.test.js
+++ __test__/components/Filter/UploadFile.test.js
@@ -3,15 +3,20 @@ import { mount } from '@vue/test-utils'
 import { i18n } from '@/global/i18n'
 import ElementPlus from 'element-plus'
 import UploadFile from '@/components/Filter/UploadFile.vue'
-import test from '@/assets/test.csv'
-import Papa from 'papaparse'
+import fs from 'fs'
+
+// 讀取文件內容
+const filePath = '__test__/upload-demo.csv' // 替換為實際文件路徑
+const fileContent = fs.readFileSync(filePath, 'utf8')
+// 創建file
+const fileData = new File([fileContent], 'upload-demo.csv', { type: 'text/csv' })
 
 describe('UploadFile', () => {
   let wrapper = null
   beforeEach(() => {
     wrapper = mount(UploadFile, {
       global: {
-        plugins: [i18n, ElementPlus, Papa]
+        plugins: [i18n, ElementPlus]
       }
     })
   })
@@ -24,25 +29,63 @@ describe('UploadFile', () => {
     // 輸入副檔名為csv的檔名，預期驗證通過
     wrapper.vm.checkCSVFile('test.csv')
     expect(wrapper.vm.isCSVFile).toBe(true)
-
     // 輸入副檔名為word的檔名，預期驗證失敗
     wrapper.vm.checkCSVFile('test.word')
     expect(wrapper.vm.isCSVFile).toBe(false)
   })
 
   // 處理選擇好的檔案
-  it('handleFileChange function', () => {
+  it('handleFileChange function', async () => {
+    // 讀取內容符合格式的檔案
     let element = {
       target: {
         files: {
-          0: {
-            name: 'test.csv'
-          }
+          0: fileData
         }
       }
     }
     wrapper.vm.handleFileChange(element)
-    expect(wrapper.vm.notOkAccountData).toBe([])
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.notOkAccountData).toStrictEqual([])
     expect(wrapper.vm.errorText).toBe('')
+
+    // 檔案為undefiend
+    let elementUndefined = {
+      target: {
+        files: {}
+      }
+    }
+    wrapper.vm.handleFileChange(elementUndefined)
+    await wrapper.vm.$nextTick()
+
+    // file undefined 執行 dialogClose
+    expect(wrapper.vm.fileName).toBe('')
+    expect(wrapper.vm.fileData).toBe('')
+    expect(wrapper.vm.notOkAccountData).toStrictEqual([])
+    expect(wrapper.vm.errorText).toBe('')
+    expect(wrapper.vm.isCSVFile).toBe(true)
+  })
+
+  // 檢查帳號
+  it('checkAccount function', () => {
+    let data = [
+      { user_name: 'Gboyfly' },
+      { user_name: 'jszch 888' },
+      { user_name: 'a123456xu' },
+      { user_name: 'hc6666' }
+    ]
+    wrapper.vm.checkAccount(data)
+
+    // 預期不合格帳號
+    expect(wrapper.vm.notOkAccountData[0]['name']).toBe('Gboyfly')
+    expect(wrapper.vm.notOkAccountData[1]['name']).toBe('jszch 888')
+  })
+
+  // 送出
+  it('handleSubmit function', () => {
+    wrapper.vm.isCSVFile = true
+    wrapper.vm.handleSubmit()
+    expect(wrapper.emitted('update:files')).toStrictEqual([[null]])
   })
 })
