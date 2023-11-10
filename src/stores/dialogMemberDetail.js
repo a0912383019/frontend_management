6bcb20b61f0e2c312c3505e1852cfda03e7fbc29
alvin_liu <alvin_liu@mail.chungyo.net>
2023-11-10 16:06:15 +0800

    update: 移除console

--- src/stores/dialogMemberDetail.js
+++ src/stores/dialogMemberDetail.js
@@ -23,7 +23,6 @@ export const useDialogMemberDetailStore = defineStore(
     })
     const showMemberDialog = ref(false)
     const updateMemberData = (val) => {
-      console.log(val)
       state.memberData = val
       sessionStorage.member_data = JSON.stringify(val)
       showMemberDialog.value = true
