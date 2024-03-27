83c1c08904b86231529088729e3f69121ed88f78
a0912383019 <a0912383019@gmail.com>
2024-03-27 14:23:46 +0800

    [test]測試staging

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { svgBuilder } from './src/utils/svgBuilder'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV !== 'staging' ? '/' : '/cdp-v2/',
+  base: import.meta.env.VITE_ENV !== 'staging' ? '/' : '/cdp-v2/',
   // build: {
   //   outDir: 'CDP'
   // },
