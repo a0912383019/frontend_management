bfb3659e80d37ad028cee4966d0d8653dbacf082
BI-CDP-Arvin_Chen <arvin_chen@mail.chungyo.net>
2024-04-09 11:02:03 +0800

    Qa us east1

---
+++ build_config/vite.config.js
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import { svgBuilder } from './src/utils/svgBuilder'
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  return defineConfig({
+    base: '/',
+    // build: {
+    //   outDir: 'CDP'
+    // },
+    plugins: [vue(), svgBuilder('./src/assets/icons/')],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/global.scss";`
+        }
+      }
+    },
+    server: {
+      proxy: {
+        '/php-api': {
+          target: 'https://qa-cdp-api.caino.club',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/php-api/, '')
+        },
+        '/go-api': {
+          target: 'https://qa-cdp-v-api.caino.club',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/go-api/, '')
+        }
+      }
+    }
+  })
+}
