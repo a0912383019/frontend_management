1d921cd16dc357fa4846e9830cf78c610884f34e
a0912383019 <a0912383019@gmail.com>
2024-07-01 10:33:37 +0800

    [add] activity analysis add dialog

--- src/api/activityAnalysis.js
+++ src/api/activityAnalysis.js
@@ -48,28 +48,3 @@ export const apiImportActivity = (params) => {
     function_id
   })
 }
-
-// export const apiListUserByAdmin = (params) => {
-//   const { hall_name, user_name, user_type, user_status, last_login_date } = params
-//   return axiosInstance.post('/api/auth/user/list_user_by_admin', {
-//     hall_name,
-//     user_name,
-//     user_type,
-//     user_status,
-//     last_login_date
-//   })
-// }
-
-// export const apiSimulateUserData = (params) => {
-//   const { user_id } = params
-//   return axiosInstance.post('/api/auth/get_simulate_user_data', {
-//     user_id
-//   })
-// }
-
-// export const apiUserByAdmin = (params) => {
-//   const { user_id_hide } = params
-//   return axiosInstance.post('/api/auth/user/query_user_by_admin', {
-//     user_id_hide
-//   })
-// }
