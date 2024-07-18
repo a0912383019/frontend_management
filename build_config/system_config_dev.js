58a8260f9e964321ae238e7ab5b20da03b6c93d2
a0912383019 <a0912383019@gmail.com>
2024-07-18 13:45:48 +0800

    [add] userAccountSetting page

--- build_config/system_config_dev.js
+++ build_config/system_config_dev.js
@@ -1208,13 +1208,13 @@ export const hall_config_dict_bbin = {
     hall_name: '金沙國際貴賓會',
     currency_sign: '¥'
   },
-  31: {
-    hall_id: 3820455,
-    domain_id: 0,
-    hall_code: '31',
-    hall_name: '银河娱乐城',
-    currency_sign: '¥'
-  },
+  // 31: {
+  //   hall_id: 3820455,
+  //   domain_id: 0,
+  //   hall_code: '31',
+  //   hall_name: '银河娱乐城',
+  //   currency_sign: '¥'
+  // },
   www: {
     hall_id: 3820454,
     domain_id: 0,
