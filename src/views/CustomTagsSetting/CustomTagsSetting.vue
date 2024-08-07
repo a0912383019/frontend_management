16e7cf0c86fafa6a42fc9b8ef6c4b32e3adfc769
a0912383019 <a0912383019@gmail.com>
2024-08-07 16:59:32 +0800

    [add] 自訂標籤設置delete 需要加上進行中不能刪除的判斷

--- src/views/CustomTagsSetting/CustomTagsSetting.vue
+++ src/views/CustomTagsSetting/CustomTagsSetting.vue
@@ -339,9 +339,9 @@ onMounted(() => {
       <template #manage="scope">
         <ButtonIcon
           class="detail-button mr-5 op-btn"
-          :disabled="scope.row.status === 3"
           color="blue"
           icon="union"
+          :disabled="scope.row.status === 3"
           :isSvg="true"
           :name="$t('custom_tags_setting.update_list')"
           @click="openImportCsv(scope.row)"
@@ -369,6 +369,7 @@ onMounted(() => {
           color="red"
           size="small"
           icon="trash"
+          :disabled="scope.row.status === 3"
           :isSvg="true"
           :name="$t('common.delete')"
           @click="openDeleteBox(scope.row)"
