8b1ef77dd2316bf32f96f90da89828f8efeed7eb
a0912383019 <a0912383019@gmail.com>
2024-08-16 11:53:09 +0800

    [update] 串接使用者帳戶api

--- src/api/adminUserList.js
+++ src/api/adminUserList.js
@@ -1,11 +1,12 @@
 import axiosInstance from './axiosInstance'
 import axiosGoInstance from './axiosGoInstance.js'
 
-// 取得當前使用者資訊
+// 取得當前使用者資訊（只需帶入token)
 export const apiQueryUserInfo = () => {
   return axiosGoInstance.get('/api/auth/user/user_info')
 }
 
+// 取得使用者列表
 export const apiListUserByAdmin = (params) => {
   const { name, user_type, user_status, last_login_date } = params
   return axiosGoInstance.get('/api/auth/admin/users', {
@@ -13,14 +14,22 @@ export const apiListUserByAdmin = (params) => {
   })
 }
 
-export const apiSimulateUserData = (params) => {
+// 等到api全部轉為golang即可移除
+// 取得使用者token資訊
+export const apiSimulateUserDataPhp = (params) => {
   const { user_id } = params
   return axiosInstance.post('/api/auth/get_simulate_user_data', {
     user_id
   })
 }
 
-// 取得使用者資訊
+// 取得使用者token資訊
+export const apiSimulateUserDataGo = (params) => {
+  const { user_id } = params
+  return axiosGoInstance.get(`/api/auth/admin/user/${user_id}/token`)
+}
+
+// 根據user_id 取得使用者資訊
 export const apiUserByAdmin = (params) => {
   const { member_id } = params
   return axiosGoInstance.get('/api/auth/admin/user/user_info', {
@@ -28,6 +37,7 @@ export const apiUserByAdmin = (params) => {
   })
 }
 
+// 更新使用者資訊
 export const apiUpdateUserByAdmin = (params) => {
   const { user_id, user_type, user_status, access_hall_name } = params
   return axiosGoInstance.put(`/api/auth/admin/user/${user_id}`, {
@@ -37,13 +47,13 @@ export const apiUpdateUserByAdmin = (params) => {
   })
 }
 
+// 刪除使用者
 export const apiDeleteUserByAdmin = (params) => {
-  const { delete_user_id_hide } = params
-  return axiosInstance.post('/api/auth/user/delete_user_by_admin', {
-    delete_user_id_hide
-  })
+  const { user_id } = params
+  return axiosGoInstance.delete(`/api/auth/admin/user/${user_id}`)
 }
 
+// 新增使用者
 export const apiCreateUserByAdmin = (params) => {
   const { email, user_type, user_status, access_hall_name } = params
   return axiosGoInstance.post('/api/auth/admin/users', {
